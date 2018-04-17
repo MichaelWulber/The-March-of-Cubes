fd2adc9a78fa30ceb1f4f3e97fe01a6acd09b457
Michael Wulber <mwulber2015@ufl.edu>
2018-04-17 16:16:15 -0400

    Add files via upload

--- ImplicitFunc.h
+++ ImplicitFunc.h
@@ -1,9 +1,13 @@
+#define GLEW_STATIC
+#include <Gl/glew.h>
+
 #ifndef IMPLICITFUNC_H
 #define IMPLICITFUNC_H
 
 class ImplicitFunc {
 public:
-	virtual bool isInside(const float &point) const = 0;
+    virtual GLfloat function(GLfloat x, GLfloat y, GLfloat z) = 0;
+    virtual bool isInside(GLfloat x, GLfloat y, GLfloat z) = 0;
 };
 
 #endif
